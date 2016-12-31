1ba0b641c6becd7f4a697c77e8d051d83e3a4067
Connie Cheng <connie.cheng52@gmail.com>
2016-12-30 23:55:48 -0500

    displaying order states with JSX

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -56,7 +56,7 @@ addToOrder(key) {
             }
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
